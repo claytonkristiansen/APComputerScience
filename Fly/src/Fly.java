4b07538971fd1d6bf29b79b803a5cf17b12d3b54
Clayton Kristiansen <clayton.kristiansen@hotmail.com>
2018-09-13 12:43:41 -0700

    Added Fly, Shapes, and Metadata

---
+++ Fly/src/Fly.java
@@ -0,0 +1,22 @@
+
+public class Fly 
+{	
+	public static void Refrain()
+	{
+		
+	}
+	public static void V1(String object)
+	{
+		System.out.print("There was an old lady who swallowed a " + object);
+	}
+
+	
+	
+	
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
